34a84eb7036a7b38df6fa3a1f75c7837437d7f4f
Edgardo Perrelli <edgardo.perrelli@plenzich.it>
2025-10-19 11:35:25 +0200

    fix: use auth-helpers client in browser to share session

--- lib/supabaseBrowser.ts
+++ lib/supabaseBrowser.ts
@@ -1,15 +1,4 @@
 'use client';
-import { createClient } from '@supabase/supabase-js';
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 
-export const supabaseBrowser = () =>
-  createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      auth: {
-        persistSession: true,
-        autoRefreshToken: true,
-        detectSessionInUrl: true,
-      },
-    }
-  );
+export const supabaseBrowser = () => createClientComponentClient();
